7d89172393535fa9c7c92a59d3c6d27c7da45aa9
shreyansh-78 <shreyanshgupta39609@gmail.com>
2024-05-08 01:27:34 +0530

    NFT Marketplace

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/T16jImFVUsmpUU4fXRdsdeOPIoGU2-al",
+      accounts: [ "752eb06d90965dd3352c6fe178fe4d826e02bb9b98021238029a8b0bcce0b4d3" ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
